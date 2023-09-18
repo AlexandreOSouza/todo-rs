56552525ac336ef241159aba82d0f6fdfe30eece
Alexandre <alexandre.o.souza12@gmail.com>
2023-09-18 20:44:06 -0300

    chore: add structs to mark, unmark and delete actions

--- src/args.rs
+++ src/args.rs
@@ -17,10 +17,13 @@ pub enum CommandEntity {
     List(ListTodo),
     /// Mark an TODO or mark all TODOs
     #[clap(short_flag = 'm')]
-    Mark,
+    Mark(MarkTodo),
     /// Unmark an TODO or unmark all TODOs
     #[clap(short_flag = 'u')]
-    Unmark,
+    Unmark(UnmarkTodo),
+    /// Delete a TODO
+    #[clap(short_flag = 'd')]
+    Delete(DeleteTodo),
 }
 
 #[derive(Debug, Args)]
@@ -34,4 +37,22 @@ pub struct ListTodo {
     pub marks: bool,
     #[arg(short = 'u')]
     pub unmarks: bool,
+}
+
+#[derive(Debug, Args)]
+pub struct MarkTodo {
+    #[arg(short = 'n')]
+    pub name: Option<String>,
+}
+
+#[derive(Debug, Args)]
+pub struct UnmarkTodo {
+    #[arg(short = 'n')]
+    pub name: Option<String>,
+}
+
+#[derive(Debug, Args)]
+pub struct DeleteTodo {
+    #[arg(short = 'n')]
+    pub name: Option<String>,
 }
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 mod args;
 
-use args::{Commands, CreateTodo, ListTodo};
+use args::{Commands, CreateTodo, DeleteTodo, ListTodo, MarkTodo, UnmarkTodo};
 use clap::Parser;
 
 use crate::args::CommandEntity;
@@ -13,11 +13,25 @@ pub fn user_handler(v: &CreateTodo) {
     println!("{:?}", v);
 }
 
+pub fn mark_todo(v: &MarkTodo) {
+    println!("{:?}", v);
+}
+
+pub fn unmark_todo(v: &UnmarkTodo) {
+    println!("{:?}", v);
+}
+
+pub fn delete_todo(v: &DeleteTodo) {
+    println!("{:?}", v);
+}
+
 fn main() {
     let args = Commands::parse();
     match args.command {
         CommandEntity::Create(v) => user_handler(&v),
         CommandEntity::List(val) => list(&val),
-        _ => println!("none"),
+        CommandEntity::Mark(v) => mark_todo(&v),
+        CommandEntity::Unmark(v) => unmark_todo(&v),
+        CommandEntity::Delete(v) => delete_todo(&v),
     }
 }
