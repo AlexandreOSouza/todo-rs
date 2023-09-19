deb1680f9fe358fc078fd801eecab661d489b996
Alexandre <alexandre.o.souza12@gmail.com>
2023-09-19 15:24:32 -0300

    chore: create the base struct for todo ations

--- src/main.rs
+++ src/main.rs
@@ -1,37 +1,20 @@
 mod args;
 
-use args::{Commands, CreateTodo, DeleteTodo, ListTodo, MarkTodo, UnmarkTodo};
+use args::Commands;
 use clap::Parser;
+use todo_rs::Todo;
 
 use crate::args::CommandEntity;
 
-pub fn list(v: &ListTodo) {
-    println!("{:?}", v);
-}
-
-pub fn user_handler(v: &CreateTodo) {
-    println!("{:?}", v);
-}
-
-pub fn mark_todo(v: &MarkTodo) {
-    println!("{:?}", v);
-}
-
-pub fn unmark_todo(v: &UnmarkTodo) {
-    println!("{:?}", v);
-}
-
-pub fn delete_todo(v: &DeleteTodo) {
-    println!("{:?}", v);
-}
-
 fn main() {
+    let mut todo = Todo::new().expect("Error creating the todo");
+
     let args = Commands::parse();
     match args.command {
-        CommandEntity::Create(v) => user_handler(&v),
-        CommandEntity::List(val) => list(&val),
-        CommandEntity::Mark(v) => mark_todo(&v),
-        CommandEntity::Unmark(v) => unmark_todo(&v),
-        CommandEntity::Delete(v) => delete_todo(&v),
+        CommandEntity::Create(new_todo) => todo.create(new_todo.name),
+        CommandEntity::List(val) => todo.list(val.marks, val.unmarks),
+        CommandEntity::Mark(v) => todo.mark(v.name),
+        CommandEntity::Unmark(v) => todo.unmark(v.name),
+        CommandEntity::Delete(v) => todo.delete(v.name),
     }
 }
